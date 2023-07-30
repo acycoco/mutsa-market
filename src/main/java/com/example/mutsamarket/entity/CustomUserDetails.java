cef6fd60f0ae58a9405b72a811a8eee21d0f9393
acycoco <pink10257@naver.com>
2023-07-30 18:22:54 +0900

    feat: CustomUserDetails 생성 loadUserByUsername, createUser, userExists 구현

---
+++ src/main/java/com/example/mutsamarket/entity/CustomUserDetails.java
@@ -0,0 +1,94 @@
+package com.example.mutsamarket.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    @Getter
+    private Long id;
+    private String username;
+    private String password;
+    @Getter
+    private String phone;
+    @Getter
+    private String email;
+    @Getter
+    private String address;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    //아래의 기능은 true로
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public static CustomUserDetails fromEntity(UserEntity entity){
+        return CustomUserDetails.builder()
+                .id(entity.getId())
+                .password(entity.getPassword())
+                .phone(entity.getPhone())
+                .email(entity.getEmail())
+                .address(entity.getAddress())
+                .build();
+    }
+
+    public UserEntity newEntity(){
+        UserEntity user = new UserEntity();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setPhone(phone);
+        user.setEmail(email);
+        user.setAddress(address);
+        return user;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomUserDetails{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='[PROTECTED]" + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/example/mutsamarket/service/JpaUserDetailsManager.java
+++ src/main/java/com/example/mutsamarket/service/JpaUserDetailsManager.java
@@ -1,14 +1,57 @@
 package com.example.mutsamarket.service;
 
+import com.example.mutsamarket.entity.CustomUserDetails;
+import com.example.mutsamarket.entity.UserEntity;
+import com.example.mutsamarket.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
+
+@Slf4j
+@Service
 public class JpaUserDetailsManager implements UserDetailsManager {
+    private final UserRepository userRepository;
+    public JpaUserDetailsManager(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserEntity> optionalUser = userRepository.findByUsername(username);
+        if (optionalUser.isEmpty()) throw new UsernameNotFoundException(username);
+        UserEntity user = optionalUser.get();
+
+        return CustomUserDetails.fromEntity(user);
+    }
+
     @Override
     public void createUser(UserDetails user) {
+        log.info("try create user : {}", user.getUsername());
+        //username 중복시 에러
+        if (userExists(user.getUsername())) throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
 
+        try {
+            //userEntity로 바꾸고 저장
+            this.userRepository.save(((CustomUserDetails)user).newEntity());
+        } catch(ClassCastException e){
+            log.error("failed to cast to {}", CustomUserDetails.class);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
+    @Override
+    public boolean userExists(String username) {
+        log.info("check if exists user : {}", username);
+        return userRepository.existsByUsername(username);
+    }
+
 
     @Override
     public void updateUser(UserDetails user) {
@@ -25,13 +68,7 @@ public class JpaUserDetailsManager implements UserDetailsManager {
 
     }
 
-    @Override
-    public boolean userExists(String username) {
-        return false;
-    }
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
-    }
+
+
 }
